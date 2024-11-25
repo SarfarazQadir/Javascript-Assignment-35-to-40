84edf59d070bb8afbbbcba55e45cb94dfb30289f
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-25 19:39:47 +0500

    "Replaced original code with commented out code blocks and added new code for a simple calculator function"

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 //  1
 
-function name() {
-    var a = new Date();
-    return console.log(a);   
-}
-name();
+// function name() {
+//     var a = new Date();
+//     return console.log(a);   
+// }
+// name();
 
 //  2
 
+// function name() {
+//  var name1 =  prompt("Your First Name");    
+//  var name2 =  prompt("Your First Name");
+//   document.write("Your full name is "+name1 + name2)
+// }
+// name()
+
+//3
+
+// function name() {
+//  var name1 =  +prompt("Your First Number 1");    
+//  var name2 =  +prompt("Your First Number 2");
+//   document.write("Added "+name1 + name2)
+// }
+// name()
+
+//4
+
+var num1 = +prompt("Enter Num 1")
+var opr = prompt("Enter Operator")
+var num2 = +prompt("Enter Num 2")
+var result;
+
+function name() {
+  if(opr == "+"){
+    result =  num1 + num2
+   }else if(opr == "-"){
+      result = num1 - num2
+   }else if(opr == "*"){
+      result = num1 * num2
+   }else if(opr == "/"){
+      result = num1 / num2
+   }
+return console.log(result);
+
+}
+name();
